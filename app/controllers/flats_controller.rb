25dc91273dfa1c30a71414f968951bfc9308656c
rocionadal <rocioonadal@gmail.com>
2020-11-25 21:09:49 +0100

    responsivnes

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -39,6 +39,6 @@ class FlatsController < ApplicationController
   end
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :image_url)
   end
 end
